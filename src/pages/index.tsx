616c74f3b7553cfdbfa258574eedcb441d3b8e51
shreyog <shreyog23@gmail.com>
2023-04-07 01:08:10 +0530

    Blog Listing (#5)
    * Changelog
    - added all website components
    
    * Changelog
    - blog setup
    
    * Changelog
    - blog listing page
    
    * Changelog
    - added blog page
    
    * Changelog
    - individual blog page update
    
    * Changelog
    - integrated posts
    
    ---------
    
    Co-authored-by: yogioctalogic <shreyog@octalogic.in>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,284 @@
 import * as React from "react";
+import v from "voca";
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
+import { Typography, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 import { useTina } from "tinacms/dist/react";
 import client from "../../tina/__generated__/client";
 
+import Link from "@/components/link/link";
+
 import IPostListProps from "@/interfaces/IPostListProps";
+
+import { HERO_POST } from "@/constants/posts";
+
+import { Post } from "@/interfaces/IPostProps";
+
+type CustomTypographyProps = {
+  size: string;
+};
+
+type CustomVariants = {
+  [key: string]: {
+    fontSize: string;
+    lineHeight: string;
+  };
+};
+
+export const BLOG_CARD_SIZE_OPTIONS = {
+  large: "large",
+  medium: "medium",
+  small: "small",
+};
+
+const TOPIC_TEXT_VARIANTS: {
+  [key: string]: {
+    fontSize: string;
+    lineHeight: string;
+  };
+} = {
+  large: {
+    fontSize: "1.625rem",
+    lineHeight: "1.875rem",
+  },
+  medium: {
+    fontSize: "1.125rem",
+    lineHeight: "1.145rem",
+  },
+  small: {
+    fontSize: ".625rem",
+    lineHeight: ".825rem",
+  },
+};
+
+const TopicText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? TOPIC_TEXT_VARIANTS[props.size as keyof {}]
+    : TOPIC_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
+  color: "#999999",
+  fontWeight: 700,
+}));
+
+const BLOG_HEADING_TEXT_VARIANTS: CustomVariants = {
+  large: {
+    fontSize: "4rem",
+    lineHeight: "4.5rem",
+  },
+  medium: {
+    fontSize: "2rem",
+    lineHeight: "2.5rem",
+  },
+  small: {
+    fontSize: "1rem",
+    lineHeight: "1.5rem",
+  },
+};
+
+const BlogHeadingText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? BLOG_HEADING_TEXT_VARIANTS[props.size as keyof {}]
+    : BLOG_HEADING_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
+  fontWeight: 700,
+}));
+
+const BLOG_SUMMARY_TEXT_VARIANTS: CustomVariants = {
+  large: {
+    fontSize: "1.75rem",
+    lineHeight: "1.95rem",
+  },
+  medium: {
+    fontSize: "1.125rem",
+    lineHeight: "1.325rem",
+  },
+  small: {
+    fontSize: ".625rem",
+    lineHeight: ".825rem",
+  },
+};
+
+const BlogSummaryText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? BLOG_SUMMARY_TEXT_VARIANTS[props.size as keyof {}]
+    : BLOG_SUMMARY_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
+  color: "#6B7280",
+}));
+
+const TAG_VARIANTS: CustomVariants = {
+  large: {
+    fontSize: "1.75rem",
+    lineHeight: "1.95rem",
+  },
+  medium: {
+    fontSize: "1.125rem",
+    lineHeight: "1.325rem",
+  },
+  small: {
+    fontSize: ".625rem",
+    lineHeight: ".825rem",
+  },
+};
+
+const Tag = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? TAG_VARIANTS[props.size as keyof {}]
+    : TAG_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
+  backgroundColor: "#D1D5DB",
+  color: "#6B7280",
+  padding: "0.625rem 1.875rem",
+}));
+
+const READ_MORE_LINK_VARIANTS: CustomVariants = {
+  large: {
+    fontSize: "1.75rem",
+    lineHeight: "1.95rem",
+  },
+  medium: {
+    fontSize: "1.125rem",
+    lineHeight: "1.325rem",
+  },
+  small: {
+    fontSize: ".625rem",
+    lineHeight: ".825rem",
+  },
+};
+
+const Tags = ({ tags, size }: { tags: string[]; size: string }) => {
+  return (
+    <Grid container alignItems="center" sx={{ gap: "0.5rem" }}>
+      {React.Children.toArray(
+        tags.map((tag: string) => <Tag size={size}>{tag}</Tag>)
+      )}
+    </Grid>
+  );
+};
+
+const truncate = ({ words = "", maxlength = 450 }) => {
+  return `${words.slice(0, maxlength)} …`;
+};
+
+export const BlogPostCard = ({ post, size }: { post: Post; size: string }) => (
+  <Grid
+    container
+    // direction="column"
+    justifyContent="space-between"
+    // sx={{minHeight: "40rem"}}
+  >
+    <Grid item xs={12}>
+      <Grid item>
+        <TopicText size={size}>
+          {v.titleCase(post?.categories?.replaceAll("-", " ") || "")}
+        </TopicText>
+      </Grid>
+      <Grid item sx={{ marginTop: "0.6rem" }}>
+        <BlogHeadingText size={size}>{post.title}</BlogHeadingText>
+      </Grid>
+      <Grid item sx={{ marginTop: "2rem" }}>
+        <BlogSummaryText size={size}>
+          {truncate({
+            words: post.summary,
+          })}
+        </BlogSummaryText>
+      </Grid>
+    </Grid>
+
+    <Grid
+      container
+      sx={{
+        flexDirection: { xs: "row-reverse", sm: "row" },
+        justifyContent: { xs: "flex-start", sm: "space-between" },
+        gap: { xs: "1rem", sm: "0" },
+        marginTop: { xs: "1rem", sm: "1.5rem" },
+      }}
+      alignItems={"flex-end"}
+    >
+      <Grid item xs={12} sm={9}>
+        {Tags({
+          tags: post.tags,
+          size: size,
+        })}
+      </Grid>
+      <Grid
+        container
+        sx={{
+          // height: "100%",
+          justifyContent: { xs: "flex-start", sm: "flex-end" },
+          alignItems: { xs: "flex-start", sm: "flex-end" },
+        }}
+        item
+        xs={12}
+        sm={3}
+      >
+        <Link
+          href={`posts/${post?._sys?.filename?.replaceAll("/posts", "")}`}
+          color="info.main"
+          sx={{
+            color: "#6B7280",
+            ...(size
+              ? READ_MORE_LINK_VARIANTS[size as keyof {}]
+              : READ_MORE_LINK_VARIANTS[
+                  BLOG_CARD_SIZE_OPTIONS.medium as keyof {}
+                ]),
+          }}
+        >
+          Read More
+        </Link>
+      </Grid>
+    </Grid>
+  </Grid>
+);
+
+const AboutOctalogicCard = () => (
+  <Grid
+    sx={{
+      padding: "1.5rem",
+      backgroundColor: "#3A9086",
+    }}
+  >
+    <Typography
+      sx={{
+        fontWeight: 700,
+        fontSize: "1.375rem",
+        lineHeight: "1.5rem",
+        color: "#F3F4F6",
+      }}
+    >
+      About
+    </Typography>
+
+    <Typography
+      sx={{
+        fontSize: "1.125rem",
+        lineHeight: "1.125rem",
+        color: "#F3F4F6",
+        marginTop: "1rem",
+      }}
+    >
+      Octalogic Tech is a SaaS company based in Goa, India, that provides custom
+      software development, mobile app development, and IT consulting services
+      to clients worldwide. Our turnkey solutions are designed to simplify the
+      process of software development and deployment, providing clients with a
+      seamless end-to-end experience.
+    </Typography>
+
+    <Typography
+      sx={{
+        fontSize: "1.125rem",
+        lineHeight: "1.125rem",
+        color: "#F3F4F6",
+        marginTop: "1rem",
+      }}
+    >
+      Being sector agnostic, we have provided tailor-made solutions to clients
+      in FinTech, MedTech, EdTech, FoodTech, and e-commerce verticals.With a
+      proven track record of delivering quality and innovative solutions to
+      clients in various industries, our team of experienced professionals
+      leverages cutting-edge technology and industry best practices that meet
+      client&apos;s specific needs and goals.
+    </Typography>
+  </Grid>
+);
 
 const InteractiveList = (props: IPostListProps) => {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -17,19 +287,47 @@ const InteractiveList = (props: IPostListProps) => {
     variables: props.variables,
     data: props.data,
   });
-  const postsList = data?.postConnection?.edges || [];
+
+  const postList = data?.postConnection?.edges || [];
+
+  const [firstPost] = postList;
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <List>
-          {postsList.map((post: any) => (
-            <ListItem key={post.node.id}>
-              <ListItemText primary={post.node._sys.filename} />
-            </ListItem>
-          ))}
-        </List>
-      </Grid>
+    <Grid className="global-spacer">
+      <Box
+        sx={{
+          paddingBlockStart: "6rem",
+          display: "grid",
+          gridTemplateColumns: { xs: "minmax(0, 1fr)", lg: "2fr 1fr" },
+          gridRowGap: { xs: "5rem", lg: "7rem" },
+          gridColumnGap: { xs: "0", lg: "7rem" },
+        }}
+      >
+        <BlogPostCard
+          post={firstPost?.node}
+          size={BLOG_CARD_SIZE_OPTIONS.large}
+        />
+        <AboutOctalogicCard />
+      </Box>
+
+      <Box
+        sx={{
+          paddingBlock: "8rem",
+          display: "grid",
+          gridTemplateColumns: {
+            xs: "minmax(0, 1fr)",
+            lg: "repeat(2, minmax(0, 1fr))",
+          },
+          gridRowGap: { xs: "5rem", lg: "7rem" },
+          gridColumnGap: { xs: "0", lg: "7rem" },
+        }}
+      >
+        {React.Children.toArray(
+          postList.map(({ node }: { node: Post }) => (
+            <BlogPostCard post={node} size={BLOG_CARD_SIZE_OPTIONS.medium} />
+          ))
+        )}
+      </Box>
     </Grid>
   );
 };
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,10 +1,26 @@
 import { GetStaticPropsContext } from "next";
+import * as React from "react";
+import { Grid, Typography, Link } from "@mui/material";
+import { format, parseISO } from "date-fns";
+
+import {
+  FiberManualRecord as FiberManualRecordIcon,
+  Facebook as FacebookIcon,
+  Twitter as TwitterIcon,
+  LinkedIn as LinkedInIcon,
+} from "@mui/icons-material";
+
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 import { useTina } from "tinacms/dist/react";
 
 import client from "../../../tina/__generated__/client";
 
+import Head from "@/components/head";
 import IPostProps from "@/interfaces/IPostProps";
+
+import { BlogPostCard, BLOG_CARD_SIZE_OPTIONS } from "@/pages";
+import { Post } from "@/interfaces/IPostProps";
 
 export default function Home(props: IPostProps) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -14,22 +30,115 @@ export default function Home(props: IPostProps) {
     data: props.data,
   });
 
+  const { data: dataList } = useTina({
+    query: props.queryList,
+    variables: props.variableList,
+    data: props.dataList,
+  });
+
+  const postList = dataList?.postConnection?.edges || [];
+
+  const post: Post = data?.post;
+
+  const seo = post?.seo;
+
   return (
-    <code>
-      <pre
-        style={{
-          backgroundColor: "lightgray",
-        }}
-      >
-        {JSON.stringify(data.post, null, 2)}
-      </pre>
-    </code>
+    <>
+      <Head title={seo?.title} description={seo?.description} />
+      <Grid className="global-spacer">
+        <Typography sx={{ paddingBlockStart: "4rem" }}>
+          {`Updated ${format(
+            parseISO(post?.postDate) || new Date(),
+            "MMM dd, yyyy"
+          )}`}
+        </Typography>
+
+        <Grid
+          container
+          sx={{
+            paddingBlock: "1rem",
+            flexDirection: { xs: "column", sm: "row" },
+          }}
+        >
+          <Grid item xs={12} sm={6}>
+            <Typography component="span">Written by</Typography>
+
+            {/* <FiberManualRecordIcon /> */}
+
+            {/* <Typography component="span">Reviewed by</Typography> */}
+          </Grid>
+
+          <Grid
+            container
+            item
+            xs={12}
+            sm={6}
+            sx={{
+              justifyContent: { xs: "flex-start", sm: "flex-end" },
+              gap: "0.5rem",
+            }}
+            alignItems={"center"}
+          >
+            <Typography>Share</Typography>
+            <Link target="_blank">
+              <FacebookIcon />
+            </Link>
+            <Link target="_blank">
+              <TwitterIcon />
+            </Link>
+            <Link>
+              <LinkedInIcon />
+            </Link>
+          </Grid>
+        </Grid>
+        <Typography
+          component={"h1"}
+          sx={{
+            fontSize: "2rem",
+            fontWeight: 700,
+            lineHeight: "3rem",
+            paddingBlockStart: "3rem",
+          }}
+        >
+          {post?.title || ""}
+        </Typography>
+        <TinaMarkdown content={post?.body || {}} />
+
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontWeight: 600,
+            fontSize: "1.2rem",
+            marginTop: "6rem",
+          }}
+        >
+          You may also like
+        </Typography>
+        <Grid
+          sx={{
+            marginBlockStart: "2rem",
+            paddingBlockEnd: "8rem",
+            display: "grid",
+            gridTemplateColumns: { xs: "1fr", lg: "repeat(3, 1fr)" },
+            gridRowGap: { xs: "5rem", lg: "7rem" },
+            gridColumnGap: { xs: "0", lg: "7rem" },
+          }}
+        >
+          {React.Children.toArray(
+            postList
+              .slice(0, 3)
+              .map(({ node: post }: { node: Post }) => (
+                <BlogPostCard post={post} size={BLOG_CARD_SIZE_OPTIONS.small} />
+              ))
+          )}
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
 export const getStaticPaths = async () => {
   const { data } = await client.queries.postConnection();
-
   if (!data?.postConnection?.edges?.length)
     return {
       paths: [],
@@ -60,11 +169,20 @@ export const getStaticProps = async (ctx: GetStaticPropsContext) => {
     relativePath: ctx.params.slug + ".md",
   });
 
+  const {
+    data: dataList,
+    query: queryList,
+    variables: variableList,
+  } = await client.queries.postConnection();
+
   return {
     props: {
       data,
       query,
       variables,
+      dataList,
+      queryList,
+      variableList,
     },
   };
 };
