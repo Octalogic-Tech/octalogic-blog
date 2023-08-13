9f2e0865925fe71ac7df3c2023509f6bf7887b15
Jude Fernandes <jude.manutd@gmail.com>
2023-08-13 10:23:16 +0530

    updated card links

--- src/components/posts/BlogPost.tsx
+++ src/components/posts/BlogPost.tsx
@@ -6,40 +6,40 @@ import { Post } from "@/interfaces/IPostProps";
 
 const BlogPostCard = ({ post }: { post?: Post }) => (
   <div className="flex flex-col justify-between flex-1 grow sm:grow-[2]">
-    <div>
+    <Link
+      prefetch={false}
+      href={`posts/${post?._sys?.filename?.replaceAll("/posts", "")}`}
+      className="text-[#6B7280] subtitle1"
+    >
       <div>
-        <h6 className="text-[#999999]">{post?.categories}</h6>
-      </div>
-      <div className="mt-[0.6rem]">
-        <h3>{post?.title}</h3>
-      </div>
-      <div className="mt-8">
-        <p className="line-clamp-6">{post?.summary}</p>
-      </div>
-    </div>
-
-    <div className="flex flex-row-reverse sm:flex-row justify-start sm:justify-between gap-4 sm:gap-0 mt-4 sm:mt-5 items-end">
-      <div className="w-full sm:w-9/12">
-        <div className="flex items-center gap-2">
-          {React.Children.toArray(
-            post?.tags.map((tag: string) => (
-              <h6 className="bg-[#DADBDD] text-[#656B78] py-[0.625rem] px-[1.875rem] rounded subtitle1">
-                {tag}
-              </h6>
-            )),
-          )}
+        <div>
+          <p className="text-[#999999]">{post?.categories}</p>
+        </div>
+        <div className="mt-[0.6rem]">
+          <h3>{post?.title}</h3>
+        </div>
+        <div className="mt-8">
+          <p className="line-clamp-6">{post?.summary}</p>
         </div>
       </div>
-      <div className="w-full sm:w-3/12 flex justify-start sm:justify-end items-start sm:items-end">
-        <Link
-          prefetch={false}
-          href={`posts/${post?._sys?.filename?.replaceAll("/posts", "")}`}
-          className="text-[#6B7280] subtitle1"
-        >
-          Read More
-        </Link>
+
+      <div className="flex flex-row-reverse sm:flex-row justify-start sm:justify-between gap-4 sm:gap-0 mt-4 sm:mt-5 items-end">
+        <div className="w-full sm:w-9/12">
+          <div className="flex items-center gap-2">
+            {React.Children.toArray(
+              post?.tags.map((tag: string) => (
+                <p className="bg-[#DADBDD] text-[#656B78] py-[0.625rem] px-[1.875rem] rounded subtitle1">
+                  {tag}
+                </p>
+              )),
+            )}
+          </div>
+        </div>
+        <div className="w-full sm:w-3/12 flex justify-start sm:justify-end items-start sm:items-end">
+          <p className="text-[#6B7280] subtitle1">Read More</p>
+        </div>
       </div>
-    </div>
+    </Link>
   </div>
 );
 
--- src/components/posts/HeroPost.tsx
+++ src/components/posts/HeroPost.tsx
@@ -6,39 +6,36 @@ import { Post } from "@/interfaces/IPostProps";
 
 const BlogPost = ({ post }: { post?: Post }) => (
   <div className="flex flex-col justify-between flex-1 grow sm:grow-[2]">
-    <div>
+    <Link href={`posts/${post?._sys?.filename?.replaceAll("/posts", "")}`}>
       <div>
-        <h5 className="text-[#999999]">{post?.categories}</h5>
-      </div>
-      <div className="mt-[0.6rem]">
-        <h1 className="text-[4rem]">{post?.title}</h1>
-      </div>
-      <div className="mt-8">
-        <p className="text-[1.6rem] line-clamp-6">{post?.summary}</p>
-      </div>
-    </div>
-
-    <div className="flex flex-row-reverse sm:flex-row justify-start sm:justify-between gap-4 sm:gap-0 mt-4 sm:mt-5 items-end">
-      <div className="w-full sm:w-9/12">
-        <div className="flex items-center gap-2">
-          {React.Children.toArray(
-            post?.tags.map((tag: string) => (
-              <h6 className="bg-[#DADBDD] text-[#656B78] py-[0.625rem] px-[1.875rem] rounded">
-                {tag}
-              </h6>
-            )),
-          )}
+        <div>
+          <h5 className="text-[#999999]">{post?.categories}</h5>
+        </div>
+        <div className="mt-[0.6rem]">
+          <h1 className="text-[4rem]">{post?.title}</h1>
+        </div>
+        <div className="mt-8">
+          <p className="text-[1.6rem] line-clamp-6">{post?.summary}</p>
         </div>
       </div>
-      <div className="w-full sm:w-3/12 flex justify-start sm:justify-end items-start sm:items-end">
-        <Link
-          href={`posts/${post?._sys?.filename?.replaceAll("/posts", "")}`}
-          className="text-[#6B7280]"
-        >
-          Read More
-        </Link>
+
+      <div className="flex flex-row-reverse sm:flex-row justify-start sm:justify-between gap-4 sm:gap-0 mt-4 sm:mt-5 items-end">
+        <div className="w-full sm:w-9/12">
+          <div className="flex items-center gap-2">
+            {React.Children.toArray(
+              post?.tags.map((tag: string) => (
+                <p className="bg-[#DADBDD] text-[#656B78] py-[0.625rem] px-[1.875rem] rounded">
+                  {tag}
+                </p>
+              )),
+            )}
+          </div>
+        </div>
+        <div className="w-full sm:w-3/12 flex justify-start sm:justify-end items-start sm:items-end">
+          <p className="text-[#6B7280]">Read More</p>
+        </div>
       </div>
-    </div>
+    </Link>
   </div>
 );
 
