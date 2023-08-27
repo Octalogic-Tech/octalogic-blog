45e26b06ecedd56ead44ce9d8d88aa14c305be6b
Jude Fernandes <jude.manutd@gmail.com>
2023-08-27 17:15:55 +0530

    updated path revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+import { revalidateTag, revalidatePath } from "next/cache";
 
 export async function POST() {
   revalidateTag("prismic");
+
+  revalidatePath("/posts/[slug]");
+  revalidatePath("/sitemap.xml");
+  revalidatePath("/");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
